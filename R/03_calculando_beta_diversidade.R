cafda8e2ba68d37ccf89c23ffe4982ef90ea1276
saramortara <saramortara@gmail.com>
2019-11-06 19:03:14 -0300

    excluindo areas com sobreposicao e criando script para regressoes

--- R/03_calculando_beta_diversidade.R
+++ R/03_calculando_beta_diversidade.R
@@ -9,36 +9,75 @@ library(dplyr)
 
 #### 1. lendo os dados ####
 ## dados do data paper CAMTRAP
-cam_files <- list.files(path="data", pattern="ATLANTIC_CAMTRAPS", 
+cam_files <- list.files(path = "data", pattern = "ATLANTIC_CAMTRAPS",
                         full.names = TRUE)
 
 head(cam_files)
 
-cam <- lapply(cam_files, read.csv)
+cam <- lapply(cam_files, read.csv, as.is = TRUE)
 names(cam) <- c('location', 'records', 'species', 'study', 'survey') 
 
 str(cam)
 
+lapply(cam, head)
+
+survey <- cam$survey
+
+head(survey)
+
+ids <- survey[,c('location_id', 'survey_id', 'effort', 'design')]
+
+# checando os tipos de desenho
+table(ids$design)
+# corrigindo os valores de desenho
+## points vira ponto
+ids$design[ids$design == 'points'] #
+ids$design[ids$design == 'points'] <- 'point'
+## vazio vira NA
+ids$design[ids$design == ''] #
+ids$design[ids$design == ''] <- NA
+## checando novamente
+table(ids$design)
+
+head(ids)
+
 ## id das areas para excluir
-areas_excluir <- read.csv("results/02_areas_excluir.csv", header = TRUE)
+dados_pred <- read.csv("results/02_preditoras_editada.csv", header = TRUE)
+#areas_excluir <- read.csv("results/02_areas_excluir.csv", header = TRUE)
+
+head(dados_pred) # location_id
+areas_ficam <- data.frame(location_id = dados_pred$location_id[!is.na(dados_pred$farming_intensity)])
+
+areas_df <- merge(areas_ficam, ids, by = 'location_id', all.x = TRUE, all.y = FALSE)
+dim(areas_ficam)
+dim(areas_df)
+
+head(areas_df)
+
+table(areas_df$location_id)
+length(unique(areas_ficam$location_id))
 
 #### 2. criando matriz area x especie para calculo da betadiversidade ####
 ### especies nas colunas e areas nas linhas
-data <- cam$records
+data <- cam$records #survey_id
 
 head(data)
 
-data_cast <- cast(data, survey_id ~ species_id, value="presence_absence")
+data_cast <- cast(data, survey_id ~ species_id, value = "presence_absence")
 rownames(data_cast) <- data_cast$survey_id
 
 head(data_cast)
 
-dim(areas_excluir)
+dim(areas_df)
+dim(data_cast)
 
 # excluindo a primeira coluna com o survey_id e as areas que nao temos dados
-area_sp <- data_cast[!data_cast$survey_id%in%areas_excluir$loctn_d,-1]
+area_sp <- data_cast[data_cast$survey_id %in% areas_df$survey_id,-1]
 
 dim(area_sp)
+dim(areas_df)
+
+rownames(area_sp)
 
 #### 3. calculo da beta diversidade #### 
 head(area_sp)
@@ -65,11 +104,15 @@ str(beta_par)
 # beta.sor = total (sorensen)
 
 ## exportando os dados para quatro planilhas diferentes, arquivos csv separados por virgula
-dir.create("results/")
-write.table(as.matrix(beta_par$beta.sim), "results/03_beta_par_turnover.csv", sep=",", col.names=NA)
-write.table(as.matrix(beta_par$beta.sne), "results/03_beta_par_nestedness.csv", sep=",", col.names=NA)
-write.table(as.matrix(beta_par$beta.sor), "results/03_beta_par_sorensen_total.csv",  sep=",", col.names=NA)
-write.table(beta_tab, "results/03_beta_total.csv", sep=",", col.names=NA)
+#dir.create("results/")
+write.table(as.matrix(beta_par$beta.sim), "results/03_beta_par_turnover.csv", 
+            sep = ",", col.names = NA)
+write.table(as.matrix(beta_par$beta.sne), "results/03_beta_par_nestedness.csv", 
+            sep = ",", col.names = NA)
+write.table(as.matrix(beta_par$beta.sor), "results/03_beta_par_sorensen_total.csv", 
+            sep = ",", col.names = NA)
+write.table(beta_tab, "results/03_beta_total.csv", 
+            sep = ",", col.names = NA)
 
 #### 4. calculando beta media e gerando tabela alfa, beta por area ####
 
@@ -78,9 +121,11 @@ colMeans(as.matrix(beta_par$beta.sim))
 beta_mean <- lapply(beta_par, function(x) colMeans(as.matrix(x)))
 
 # criando data.frame com valores de beta medias
-div_df <- as.data.frame(matrix(unlist(beta_mean), ncol=3, nrow=162, byrow = FALSE))
+div_df <- as.data.frame(matrix(unlist(beta_mean), 
+                               ncol = 3, nrow = nrow(area_sp), byrow = FALSE))
   
 div_df$survey_id <- rownames(area_sp)
+names(div_df)[1:3] <- c("beta.sim", "beta.sne", "beta.sor")
 
 head(div_df)
 
@@ -88,8 +133,31 @@ div_df$alfa <- rowSums(area_sp)
 
 head(div_df)
 
-names(div_df)[1:3] <- c("beta.sim", "beta.sne", "beta.sor")
-
 write.table(div_df, "results/03_diversity.csv", 
             col.names = TRUE, 
-            row.names=FALSE)
+            row.names = FALSE)
+
+#### 5. juntando todos os dados ####
+head(ids)
+
+## primeiro junta com o id
+div_ids <- merge(div_df, ids, by = 'survey_id')
+
+dim(div_ids)
+
+head(div_ids)
+
+## agora juntamos com as variaveis preditoras
+head(dados_pred)
+dim(div_ids)
+
+dados_geral <- merge(div_ids, dados_pred[,-1], by = 'location_id')
+
+head(dados_geral)
+
+write.table(dados_geral, 
+            "data/dados_geral.csv", 
+            col.names = TRUE, row.names = FALSE, sep = ",")
+
+
+apply(dados_geral, 2, function(x) sum(is.na(x)))
---
+++ R/04_script_analise_regressão.R
@@ -0,0 +1,222 @@
+##### An?lise de regress?o linear #####
+
+# carregando os pacotes
+library(lme4) # modelos mistos
+library(bbmle) # AICtab
+library(scales) # para rescalonar variavel effort
+library(ggplot2) # para graficos
+
+####criando o diretorio#### BAIXAR NOVOS DOCUMENTOS
+#setwd("C:\\Users\\beatr\\Desktop\\dissertacao_bia-master\\results")
+getwd()
+
+###USAR dados FORMADOS EM "BETADIVERSIDADE E MANEJO"###
+#lendo os dados - transformar planilhas em objetos
+dados <- read.csv("data/dados_geral.csv", header = TRUE, sep = ",")
+
+head(dados)
+
+
+##########################################
+### DISTRIBUICAO DAS VARIAVEIS  ##########
+##########################################
+
+##histograma
+hist(dados$farming_intensity)
+
+##adicionar linha de media
+abline(v = mean(dados$farming_intensity, na.rm = TRUE), col = "green", lwd = 3)
+
+##histograma
+### beta total
+hist(dados$beta.sor)
+abline(v = mean(dados$beta.sor, na.rm = TRUE), col = "green", lwd = 3)
+
+### beta substituicao
+hist(dados$beta.sim)
+abline(v = mean(dados$beta.sim, na.rm = TRUE), col = "green", lwd = 3)
+
+### beta aninhamento
+hist(dados$beta.sne)
+abline(v = mean(dados$beta.sne, na.rm = TRUE), col = "green", lwd = 3)
+
+###rela??o entre as variaveis
+#par(mfrow = c(1, 3))
+plot(beta.sor ~ farming_intensity, data = dados, las = 1 , bty = "l", main = "Total")
+plot(beta.sim ~ farming_intensity, data = dados, las = 1 , bty = "l", main = "Turnover")
+plot(beta.sne ~ farming_intensity, data = dados, las = 1 , bty = "l", main = "Aninhamento")
+plot(alfa ~ farming_intensity, data = dados, las = 1 , bty = "l", main = "Riqueza")
+
+#par(mfrow = c(1, 1))
+
+
+###############################
+#### CONSRTU??O DE MODELOS ####
+###############################
+
+##criar modelo linear e guardar como objeto - usa as duas v?riaveis
+
+##criar objetos para representar as hipoteses
+
+head(dados)
+
+sort(dados$effort)
+dados$effort2 <- rescale(dados$effort)
+
+dados2 <- dados[!is.na(dados$farming_intensity),]
+dim(dados2)
+
+dim(dados)
+
+apply(dados, 2, function(x) sum(is.na(x)))
+
+# Modelos para beta total
+h1.1 <- lmer(beta.sor ~ farming_intensity + (1|location_id), 
+             # weights = dados$effort2, 
+             data = dados)
+
+h0.1 <- lmer(beta.sor ~ 1 + (1|location_id), 
+             #weights = effort2, 
+             data = dados)
+
+AICctab(h1.1, h0.1)
+
+# Modelos para beta turnover
+h1.2 <- lmer(beta.sim ~ farming_intensity + (1|location_id), 
+             #weights = effort2, 
+             data = dados)
+
+h0.2 <- lmer(beta.sim ~ 1 + (1|location_id), 
+             #weights = effort2, 
+             data = dados)
+
+AICctab(h1.2, h0.2)
+
+# Modelos para alfa diversidade
+h1.3 <- glmer(alfa ~ farming_intensity + (1|location_id), 
+              #weights = effort2, 
+              family = 'poisson',
+              data = dados)
+
+h0.3 <- glmer(alfa ~ 1 + (1|location_id), 
+              #weights = effort2, 
+              family = 'poisson',
+              data = dados)
+
+AICctab(h1.3, h0.3)
+
+### Exemplo de grafico para a riqueza
+
+# criando objetos para auxiliar grafico
+## tamanho do texto e numeros
+tamt <- 18
+## tamanho dos pontos
+tamp <- 3
+
+r <- ggplot(dados, aes(x = farming_intensity, y = alfa)) +
+  geom_smooth(method = lm, fill = "grey80", se = TRUE) +
+  geom_point(shape = 19, size = tamp, alpha = 0.5) + # size=3
+  labs(x = "Intensidade do manejo", y = "Riqueza)") +
+  #scale_color_manual(values = cor) +
+  theme_classic(base_size = tamt)
+
+r
+
+
+
+###### a partir daqui não roda ##########################
+#Hipotese 2 - o tamanho do fragmento, junto com o tipo de matriz, interfere na riqueza de esp?cies
+h2<- glm(riqueza~log(Size)+Vegetation.type, data = floresta, family = 'poisson')
+
+#Hipotese 3 - nula
+h3<- glm(riqueza~1, data = floresta, family= 'poisson')
+
+##sumario dos modelos
+
+#sumario hipotese 1
+summary(h1)
+
+#sumario hipotese 2
+summary(h2)
+
+#sumario hipotese 3
+summary(h3)
+
+##############################
+#### INSPE??O DE RESIDUOS ####
+##############################
+
+##mudar a conforma??o
+par(mfrow=c(2,2))
+
+#inspes?o de cada modelo - gerando o plot do modelo
+
+#residuos hipotese 1
+plot(h1)
+
+#residuos hipotese 2
+plot(h2)
+
+#residuos hipotese 3
+plot(h3)
+
+##voltando a conforma??o
+par(mfrow=c(1,1))
+
+#######################################
+#### COMPARANDO OS MODELOS COM AIC ####
+#######################################
+
+library(bbmle)
+
+AICtab(h1, h2, h3, weights=TRUE, base=TRUE)
+
+summary(h1)
+
+###############################################
+#### COMPARAR AJUSTE DO MODELO COM GRAFICO ####
+###############################################
+
+##para consiguir identificar onde inicia a sequencia/termina
+range(log(floresta$Size),na.rm = TRUE)
+
+##criar a sequencia para eixo x
+xv<- seq(-2,14, length= 294)
+
+##predict - necessario para ver como seria o modelo e como com os dados reais
+
+##fun??o range - serve para verificar o tamanho da do primeiro numero do eixo x e ultimo
+range(log(floresta$Size),na.rm=TRUE)
+
+##criar um novo data para ser o objeto Size
+data.new <- data.frame(Size=xv)
+
+##fazer a predi??o
+predh1<- predict(h1, newdata=data.new, type="response")
+
+#fun??o length serve para ver variaveis tem ao redor
+length(predh1)
+
+#sumario de predict
+summary(predh1)
+
+#criar um objeto com o expoente de coef de h1, para poder plotar os dados e fazer a curva do modelo
+coefh1<- exp(coef(h1))
+
+##plot dos dados riqueza e tamanho frag (log)
+
+plot(riqueza ~ log(Size), data=floresta, col=floresta$cor,las=1, 
+     xlab="Size(log)", ylab="Riqueza", pch=1, bty= 'l')
+
+##curva do modelo. A fun??o 'curve' ? fun??o, ent?o precisar ter dados
+curve(coefh1[1]+ coefh1[2]*x, add = TRUE)
+
+###incluindo legenda
+legend("topright", c("Ombr?fila", "Semidecidua", "Decidua"), 
+       col=c("darkgreen", "green", "orange"), pch=1)
+
+#retirando pontos de camptrap
+camtrap.novo <- camtrap[c(-2,-3,-6,-9,-10,-11,-17,-19,-24,-25,-27,-29,-42,-43,-58,-62,-80,-81,-83,-86,-92,-94,-105,-107,-108,-139,-141,-144),]
+
+head(camtrap.novo)
+tail(camtrap.novo)
